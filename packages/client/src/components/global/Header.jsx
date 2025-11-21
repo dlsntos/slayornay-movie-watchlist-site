0ae6132bf689f9ae19efcec67ceebab20cc89889
Dean <deanlt.santos@gmail.com>
2025-11-21 13:30:33 +0800

    create header structure

---
+++ packages/client/src/components/global/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <header>
+      <div></div>
+      <nav>
+        <div>
+          <input type="text" placeholder="Search" />
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>Popular</li>
+          <li>New</li>
+          <li>Watchlist</li>
+        </ul>
+        <button>Sign in</button>
+      </nav>
+    </header>
+  );
+};
+export default Header;
